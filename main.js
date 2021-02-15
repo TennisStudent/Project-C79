90f7f80aa908afa7575355488102cd20a87472b0
TennisStudent <73370352+TennisStudent@users.noreply.github.com>
2021-02-15 17:57:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function getmenu(){
     htmldata="<ol class='menulist'>"
     menu_list_array.sort();
     for(var i=0;i<menu_list_array.length;i++){
-        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+        htmldata=htmldata+"<li>" + menu_list_array[i] + "</li>"
     }
     htmldata=htmldata+"</ol>"
     document.getElementById("display_menu").innerHTML = htmldata;
@@ -20,7 +20,7 @@ function add_item(){
     menu_list_array.sort();
     htmldata="<section class='cards'>"
     for(var i=0;i<menu_list_array.length;i++){
-        htmldata=htmldata+'<div class="card">' + '<img src="pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+        htmldata=htmldata+"<div class='cards'>" + "<img src='pizzaImg.png'>" + menu_list_array[i] + "</div>"
     }
     htmldata=htmldata+"</section>"
     document.getElementById("display_addedmenu").innerHTML = htmldata;
